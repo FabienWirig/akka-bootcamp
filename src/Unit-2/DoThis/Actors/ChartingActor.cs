b8f6117d2fdb91a761ce3d9da1fce36eb3758cae
FabienW <fabien.wirig@gmail.com>
2017-01-30 23:03:31 +0100

    Unit 2 - Lesson 4

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
 
@@ -49,25 +50,31 @@ namespace ChartApp.Actors
 
             public string SeriesName { get; private set; }
         }
+        public class TogglePause { }
 
         #endregion
 
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
+        private readonly Button _pauseButton;
 
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
+        public ChartingActor(Chart chart, Button pauseButton)
+            : this(chart, new Dictionary<string, Series>(), pauseButton)
         {
         }
 
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex,
+            Button pauseButton)
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
+            _pauseButton = pauseButton;
+            Charting();
+        }
 
-            Receive<InitializeChart>(ic => HandleInitialize(ic));
-            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries)); 
-            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
-            Receive<Metric>(metric => HandleMetrics(metric));
+        private void SetPauseButtonText(bool paused)
+        {
+            _pauseButton.Text = string.Format("{0}", !paused ? "PAUSE ||" : "RESUME ->");
         }
 
 
@@ -140,7 +147,45 @@ namespace ChartApp.Actors
             }
         }
 
+        private void HandleMetricsPaused(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series)
+                && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                // set the Y value to zero when we're paused
+                series.Points.AddXY(xPosCounter++, 0.0d);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
+
         #endregion
+
+        private void Charting()
+        {
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
+
+            //new receive handler for the TogglePause message type
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(true);
+                BecomeStacked(Paused);
+            });
+        }
+
+        private void Paused()
+        {
+            Receive<Metric>(metric => HandleMetricsPaused(metric));
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(false);
+                UnbecomeStacked();
+            });
+        }
 
         private void SetChartBoundaries()
         {
